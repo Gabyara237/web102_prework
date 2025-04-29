9d7e2295485afd763c55f36f88ecc6c55a26309d
Gabriela Araujo <gabyara237@gmail.com>
2025-04-28 22:25:15 -0700

    Challenge 5: Add functions to filter the funded and unfunded games

--- index.js
+++ index.js
@@ -94,8 +94,9 @@ function filterUnfundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have not yet met their goal
-
+  const result = GAMES_JSON.filter((game) => game.pledged < game.goal);
   // use the function we previously created to add the unfunded games to the DOM
+  addGamesToPage(result);
 }
 
 // show only games that are fully funded
@@ -103,8 +104,10 @@ function filterFundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have met or exceeded their goal
+  const result = GAMES_JSON.filter((game) => game.pledged > game.goal);
 
   // use the function we previously created to add unfunded games to the DOM
+  addGamesToPage(result);
 }
 
 // show all games
@@ -112,6 +115,7 @@ function showAllGames() {
   deleteChildElements(gamesContainer);
 
   // add all games from the JSON data to the DOM
+  addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -120,6 +124,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
